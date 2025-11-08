0db082adb7d63cfccd1d0c76ed59e6c37a562ecd
matansuliman <matan.2001@gmail.com>
2025-11-08 16:22:26 +0200

    add max age 0

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -37,7 +37,7 @@ def create_flask_app(env= 'development'):
 
     @app.after_request
     def no_cache(response):
-        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
         response.headers["Pragma"] = "no-cache"
         response.headers["Expires"] = "0"
         return response
