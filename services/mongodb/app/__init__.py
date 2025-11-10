a1c9d7d2dd26b8416dda25ee1db9d94e77d710ae
matansuliman <matan.2001@gmail.com>
2025-11-10 10:49:36 +0200

    import mongodb manager and init client

--- services/mongodb/app/__init__.py
+++ services/mongodb/app/__init__.py
@@ -3,11 +3,14 @@ import os
 from flask import Flask
 
 from .managers.formatter_management import configure_logging
+from .managers.mongodb_management import MongoDBManager
 
 def create_flask_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
     configure_logging(app)
+
+    MongoDBManager.init_client()
 
     # Register Blueprints
     from .routes import bp
