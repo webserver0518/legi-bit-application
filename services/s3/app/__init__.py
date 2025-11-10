05403c9e623075e3db82a6cc412e32306277d415
matansuliman <matan.2001@gmail.com>
2025-11-10 11:39:17 +0200

    register blueprints import inside create app function

--- services/s3/app/__init__.py
+++ services/s3/app/__init__.py
@@ -1,10 +1,13 @@
 # app/__init__.py
 import os
 from flask import Flask
-from .routes import bp
 
 def create_flask_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
+    
+    # Register Blueprints
+    from .routes import bp
     app.register_blueprint(bp)
+
     return app
