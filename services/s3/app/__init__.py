41d381fcf0c37742ac3643e59c8d8055d14eb35f
matansuliman <matan.2001@gmail.com>
2025-11-10 12:00:42 +0200

    import s3 manager and init inside create app function

--- services/s3/app/__init__.py
+++ services/s3/app/__init__.py
@@ -3,11 +3,14 @@ import os
 from flask import Flask
 
 from .managers.formatter_management import configure_logging
+from .managers.s3_management import S3Manager
 
 def create_flask_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
     configure_logging(app)
+
+    S3Manager.init()
     
     # Register Blueprints
     from .routes import bp
