0a02f2b2e9d4e57e6b720d9d82a6f03ab052b9af
matansuliman <matan.2001@gmail.com>
2025-11-10 11:39:54 +0200

    import configure logging and call it on app

--- services/s3/app/__init__.py
+++ services/s3/app/__init__.py
@@ -2,9 +2,12 @@
 import os
 from flask import Flask
 
+from .managers.formatter_management import configure_logging
+
 def create_flask_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
+    configure_logging(app)
     
     # Register Blueprints
     from .routes import bp
