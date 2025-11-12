c67c0d1edb5f2de5ec4827e303986b1f35c8ab39
matansuliman <matan.2001@gmail.com>
2025-11-12 22:28:22 +0200

    disable all loggings

--- services/mongodb/app/__init__.py
+++ services/mongodb/app/__init__.py
@@ -2,13 +2,14 @@
 import os
 from flask import Flask
 
-from .managers.formatter_management import configure_logging
+from .managers.formatter_management import configure_logging, disable_all_logging
 from .managers.mongodb_management import MongoDBManager
 
 def create_flask_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
     configure_logging(app)
+    disable_all_logging(app)
 
     MongoDBManager.init()
 
