bc83897a88c3a18f4202f34fbb4c98ba269441bf
matansuliman <matan.2001@gmail.com>
2025-11-20 13:54:01 +0200

    delete console log on every check, add console log on expired and error cases

--- flask/app/static/js/core/session_monitor.js
+++ flask/app/static/js/core/session_monitor.js
@@ -5,14 +5,15 @@
                 method: "GET",
                 credentials: "include"
             });
-            console.log(res.status)
 
             if (res.status === 401) {
+                console.log("Session expired, reloading...");
                 window.Toast.warning("⚠️ תוקף ההתחברות פג. אנא התחבר שוב.", { sticky: true });
                 setTimeout(() => location.reload(true), 2000);
             }
 
         } catch (err) {
+            console.log("Error pinging session:", err);
             console.warn("Ping failed", err);
         }
     }
